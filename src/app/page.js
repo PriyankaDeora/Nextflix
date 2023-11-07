24a97e63e95ef287f369c2f240a841c7c9b3cd71
Priyanka <deorapriyanka202@gmail.com>
2023-11-07 15:16:33 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Herosection from './components/Herosection'
+
+function Home() {
+  return (
+    <>
+        <Herosection title={"LET'S WATCH MOVIE TOGETHER"} imageUrl={"/home.svg"} />
+    </>
+  )
+}
+
+export default Home
